9fd2c4c976b70d42523fa05e3936f5063a2a6d25
APThierry <thierrysousa2011@gmail.com>
2025-10-03 14:33:08 -0300

    hemp 1

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-const path = require('path');
-
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.resolve(__dirname, 'node_modules/react'),
-      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'react/jsx-runtime': path.resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
-      'react/jsx-dev-runtime': path.resolve(__dirname, 'node_modules/react/jsx-dev-runtime.js')
-    };
-
-    return config;
-  }
 };
+
+module.exports = nextConfig;
